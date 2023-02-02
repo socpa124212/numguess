af8a80f59a9aa8a78b1258d2c499597edfee952a
socpa124212 <socpa1234@kau.kr>
2023-02-02 12:27:46 +0900

    feat : get username, guess from user using input

--- numguess.py
+++ numguess.py
@@ -1,7 +1,14 @@
 from random import randint
 
-#create answer range : 1 to 100 (integer)
+# create answer range : 1 to 100 (integer)
 answer = randint(1,100)
-#print answer(for debugging)
+
+# print answer(for debugging)
 print(answer)
 
+# get user's name, guess
+user_name = input("hello, there! what is your name?")
+guess = input(f'Hi, {user_name} guess the number between 1 ~ 100 : ')
+
+# print to check
+print(user_name, guess)
